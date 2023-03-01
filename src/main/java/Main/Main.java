4d466e2b97f79e342f3c869e53a05259aae6b343
Abdenour Touat <61945965+abdennour-touat@users.noreply.github.com>
2023-03-01 20:27:50 +0100

    reseting

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -4,6 +4,7 @@ package Main;
 public class Main {
     public static void main(String[] args) {
         //subset problem.
+        System.out.println("hello");
         //model the problem
         //  instantiate the problem
         // verification algorithm
